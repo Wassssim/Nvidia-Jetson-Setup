592a4d59154357eada555333a67560665cacea66
Wassssim <aprsn124@gmail.com>
2021-12-14 14:00:08 +0100

    Added startup script

---
+++ install_additional.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#pip3 install gdown
+
+#for kafka
+sudo apt-get install -y libglib2.0 libglib2.0-dev
+sudo apt-get install -y libjansson4  libjansson-dev
+sudo apt-get install -y librdkafka1=0.11.3-1build1
+
+#for autostartup
+sudo cp rc-local.service /etc/systemd/system/
+sudo cp "rc.local" /etc/
+sudo chmod +x /etc/rc.local
+sudo systemctl enable rc-local
+
+sudo apt install libjson-glib-dev
