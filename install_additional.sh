de685dfc5a977faba67e40f4c68f91b1f7bc311a
Wassssim <aprsn124@gmail.com>
2022-01-28 16:22:48 +0100

    Added lsof and autossh installs

--- install_additional.sh
+++ install_additional.sh
@@ -8,9 +8,11 @@ sudo apt-get install -y libjansson4  libjansson-dev
 sudo apt-get install -y librdkafka1=0.11.3-1build1
 
 #for autostartup
-sudo cp rc-local.service /etc/systemd/system/
-sudo cp "rc.local" /etc/
-sudo chmod +x /etc/rc.local
-sudo systemctl enable rc-local
+#sudo cp rc-local.service /etc/systemd/system/
+#sudo cp "rc.local" /etc/
+#sudo chmod +x /etc/rc.local
+#sudo systemctl enable rc-local
 
-sudo apt install libjson-glib-dev
+sudo apt install -y lsof
+sudo apt install -y autossh
+sudo apt install -y libjson-glib-dev
